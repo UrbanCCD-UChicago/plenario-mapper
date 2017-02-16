4c48718b5e248ceb5660c19910fcb849b6337980
Ubuntu <ubuntu@ip-10-0-1-248.ec2.internal>
2017-02-16 20:29:48 +0000

    Set default log level to info

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.consumerToken = process.env.CONSUMER_TOKEN;
 config.postgresUri = process.env.POSTGRES_URI;
 config.redshiftUri = process.env.REDSHIFT_URI;
 config.logFile = process.env.LOG_FILE || 'mapper.log';
-config.logLevel = process.env.LOG_LEVEL || 'error';
+config.logLevel = process.env.LOG_LEVEL || 'info';
 config.plenarioHost = process.env.PLENARIO_HOST || 'localhost:8080';
 config.publisherUrl = process.env.PUBLISHER_URL || 'http://streaming.plenar.io/';
 
